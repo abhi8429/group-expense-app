b012321264408926c0647bbf94edb30e5ea1dd4e
abhishek <aksinghabhishek3155@gmail.com>
2024-09-30 21:22:00 +0530

    added login,register,dashboard,expense,group components

--- group-expense-app/src/app/app.module.ts
+++ group-expense-app/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { GroupListComponent } from './group/group-list/group-list.component';
+import { GroupDetailComponent } from './group/group-detail/group-detail.component';
+import { ExpenseListComponent } from './expense/expense-list/expense-list.component';
+import { ExpenseDetailComponent } from './expense/expense-detail/expense-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    GroupListComponent,
+    GroupDetailComponent,
+    ExpenseListComponent,
+    ExpenseDetailComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
