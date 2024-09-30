153684c02020778801b54471170e5ec7c92675e0
abhishek <aksinghabhishek3155@gmail.com>
2024-09-30 22:07:34 +0530

    added new application

--- group-expense-manager/src/app/app.component.spec.ts
+++ group-expense-manager/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'group-expense-app'`, () => {
+  it(`should have as title 'group-expense-manager'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('group-expense-app');
+    expect(app.title).toEqual('group-expense-manager');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('group-expense-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('group-expense-manager app is running!');
   });
 });
